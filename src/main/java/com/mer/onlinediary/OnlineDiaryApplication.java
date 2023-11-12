4b52fcc103b8cdc74aef18b2872c881047c68b80
lenaaMorozz <lenaamorozz@gmail.com>
2023-11-12 17:20:51 +0500

    init

---
+++ src/main/java/com/mer/onlinediary/OnlineDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.mer.onlinediary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineDiaryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mer/onlinediary/OnlineDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mer.onlinediary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineDiaryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
